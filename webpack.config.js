9d84fc2f644291784840990f3e3df6469e3503d7
fun <2419584260@qq.com>
2018-03-08 15:08:36 +0800

    修改webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry:{
-        index:'./src/index.js'
+        index:'./src/index.js',
+        vendor:['react','react-dom']
     },
     output:{
         filename:'[name].js',
@@ -17,9 +20,33 @@ module.exports = {
             },
             {
                 test:/\.css$/,
-                loader: "style-loader!css-loader"
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
             }
         ]
 
-    }
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            output: {
+                comments: false,
+            },
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        }),
+        new webpack.optimize.CommonsChunkPlugin({name:'vendor',filename:'vendor.bundle.js'}),
+        new HtmlWebpackPlugin({
+            title: 'redux',
+            filename: '../index.html',
+            template: './template.html',
+            chunks:['index','vendor']
+        }),
+        new ExtractTextPlugin("styles.css")
+    ]
 };
